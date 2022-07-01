f4223a061d9b35f1d4d8a8d99ccb672f5547d5d5
Aarseus <trojanbista@gmail.com>
2022-07-01 13:33:51 +0545

    api key hidden

--- src/App.js
+++ src/App.js
@@ -9,21 +9,22 @@ import {
   Route,
 } from "react-router-dom";
 export default class App extends Component {
-  pageSize=10
+  pageSize=10;
+  apiKey=process.env.REACT_APP_NEWS_API
   render() {
     return (
       <>
       <Router>
       <Navbar/>
       <Routes>
-        <Route exact  path='/' element={<News key={'general'} pageSize={this.pageSize} category={'general'}/>}/>
-        <Route exact  path='/general' element={<News key={'general'} pageSize={this.pageSize} category={'general'}/>}/>
-        <Route exact path='/business'element={<News key={'business'} pageSize={this.pageSize} category={'business'}/>}/>
-        <Route exact path='/sports'element={<News key={'sports'} pageSize={this.pageSize} category={'sports'}/>}/>
-        <Route exact path='/technology'element={<News  key={'technology'}pageSize={this.pageSize} category={'technology'}/>}/>
-        <Route exact path='/health'element={<News key={'health'} pageSize={this.pageSize} category={'health'}/>}/>
-        <Route exact path='/science'element={<News  key={'science'}pageSize={this.pageSize} category={'science'}/>}/>
-        <Route exact path='/entertainment'element={<News key={'entertainment'} pageSize={this.pageSize} category={'entertainment'}/>}/>
+        <Route exact  path='/' element={<News key={'general'} apiKey={this.apiKey} pageSize={this.pageSize} category={'general'}/>}/>
+        <Route exact  path='/general' element={<News key={'general'} apiKey={this.apiKey} pageSize={this.pageSize} category={'general'}/>}/>
+        <Route exact path='/business'element={<News key={'business'} apiKey={this.apiKey} pageSize={this.pageSize} category={'business'}/>}/>
+        <Route exact path='/sports'element={<News key={'sports'} apiKey={this.apiKey} pageSize={this.pageSize} category={'sports'}/>}/>
+        <Route exact path='/technology'element={<News  key={'technology'} apiKey={this.apiKey}pageSize={this.pageSize} category={'technology'}/>}/>
+        <Route exact path='/health'element={<News key={'health'} apiKey={this.apiKey} pageSize={this.pageSize} category={'health'}/>}/>
+        <Route exact path='/science'element={<News  key={'science'} apiKey={this.apiKey}pageSize={this.pageSize} category={'science'}/>}/>
+        <Route exact path='/entertainment'element={<News key={'entertainment'} apiKey={this.apiKey} pageSize={this.pageSize} category={'entertainment'}/>}/>
       </Routes>
   
       </Router>
--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -27,7 +27,7 @@ export class News extends Component {
 
     async updateNews(){
      
-        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b3a14e2aaef045a3b41867fa05fdffad&&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&&page=${this.state.page}&pageSize=${this.props.pageSize}`
         this.setState({loading: true})
         let data= await fetch(url);
         let parsedData=await data.json()
